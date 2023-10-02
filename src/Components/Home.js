deab98cbf4051a04297b6563dc26488e10922762
Cedric <cedricj765@mail.com>
2023-10-02 12:52:41 -0500

    home page updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <div>
         <br />
-        <p>This will load my Home Page content</p>
+        <h1><em>Welcome to the most popular auction in town!</em></h1>
+        <br />
+          <h2>Shop around or register your car to be auctioned! While you wait for your car to get sold, take a look at some of these videos displaying the most iconic muscle cars from the 60s and 70s.</h2>
     </div>
   )
 }
