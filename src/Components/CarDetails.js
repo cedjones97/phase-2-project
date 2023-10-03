40d63a003f7ef85bbf5e6f2339197ee8ab396933
Cedric <cedricj765@mail.com>
2023-10-03 11:29:30 -0500

    CarDetail componenet updated

--- src/Components/CarDetails.js
+++ src/Components/CarDetails.js
@@ -1,8 +1,31 @@
 
 
-const CarDetails  = () => {
+const CarDetails  = ({
+    carName,
+    image,
+    description,
+    engine,
+    trans,
+    drivetrain,
+    power,
+    torque,
+    speed,
+    quarter
+}) => {
+
   return (
-    <h1>I am CarDetails</h1>
+    <div className='card'>
+      <img src={image} alt='muscle-car'/>
+      <h1>{carName}</h1>
+      <p>{description}</p>
+      <p>{engine}</p>
+      <p>{trans}</p>
+      <p>{drivetrain}</p>
+      <p>{power}</p>
+      <p>{torque}</p>
+      <p>{speed}</p>
+      <p>{quarter}</p>
+    </div>
   )
 }
 
