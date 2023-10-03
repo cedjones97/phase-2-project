ce58183b9224a3db2223db5d70f6b962ef51e381
Cedric <cedricj765@mail.com>
2023-10-03 17:54:09 -0500

    navBar css updated

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,16 +1,7 @@
 import React, { NavLink } from 'react-router-dom'
 import Nav from '../Nav.css'
 
-const linkStyles = {
-  display: "inline-block",
-  width: "150px",
-  padding: "12px",
-  margin: "12px 12px 12px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-  
-};
+
 
 const NavBar = () => {
   return (
@@ -18,17 +9,14 @@ const NavBar = () => {
         <NavLink 
         to='/' 
         exact 
-        style={linkStyles}
         >Home</NavLink>
         <NavLink 
         to='/auction' 
         exact 
-        style={linkStyles}
         >Auction</NavLink>
         <NavLink 
         to='/auctionyourcar' 
         exact 
-        style={linkStyles}
         >Auction Your Car</NavLink>
     </div>
   )
