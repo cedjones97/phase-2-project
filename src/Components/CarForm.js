2bc5973207db5dc6f105ae27d99cd244f7d6eb08
Cedric <cedricj765@mail.com>
2023-10-06 04:21:28 -0500

    POSt successful

--- src/Components/CarForm.js
+++ src/Components/CarForm.js
@@ -23,35 +23,74 @@ const CarForm = ({ count, handleForwardCount, handleBackwardCount, handlePost })
     //     handleBackwardCount(count)
     // }
     const handleEngine = (e) => {
-
       setEngine(e.target.value)
     }
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleCarName = (e) => {
+      setCarName(e.target.value)
+    }
 
-        const itemData = {
-          engine: engine,
-          transmission: '',
-          drivetrain: '',
-          power: '',
-          torque: '',
-          mph: '',
-          quarterMile: '',
-        }
+    const handleImage = (e) => {
+      setImage(e.target.value)
+    }
 
-        fetch('http://localhost:3000/carData', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(itemData),
-        })
+    const handleDescription = (e) => {
+      setDescription(e.target.value)
+    }
 
-        .then(res => res.json())
-        .then(newCar => console.log(newCar))
+    const handleTransmission = (e) => {
+      setTransmission(e.target.value)
+    }
 
-      }
+    const handleDrivetrain = (e) => {
+      setDrivetrain(e.target.value)
+    }
+
+    const handlePower = (e) => {
+      setPower(e.target.value)
+    }
+
+    const handleTorque = (e) => {
+      setTorque(e.target.value)
+    }
+
+    const handleQuarterMile = (e) => {
+      setQuarterMile(e.target.value)
+    }
+
+    const handleMph = (e) => {
+      setMph(e.target.value)
+    }
+
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const itemData = {
+      carName: carName,
+      image: image,
+      description: description,
+      engine: engine,
+      transmission: transmission,
+      drivetrain: drivetrain,
+      power: power,
+      torque: torque,
+      mph: mph,
+      quarterMile: quarterMile,
+    }
+
+    fetch('http://localhost:3000/carData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(itemData),
+      })
+
+      .then(res => res.json())
+      .then(newCar => handlePost(newCar))
+
+    }
   
 
   return (
@@ -60,16 +99,16 @@ const CarForm = ({ count, handleForwardCount, handleBackwardCount, handlePost })
         <br />
         <form onSubmit={handleSubmit}>
             <div className='field1'>
-            <input placeholder='vehicle name..' value={engine} onChange={handleEngine}/>
-            <input placeholder='vehicle image..'/>
-            <input placeholder='vehicle description..'/>
-            <input placeholder='vehicle engine type..'/>
-            <input placeholder='vehicle transmission type..'/>
-            <input placeholder='vehicle drivetrain type..'/>
-            <input placeholder='vehicle power..'/>
-            <input placeholder='vehicle torque..'/>
-            <input placeholder='vehicle 0-60..'/>
-            <input placeholder='vehicle 1/4 split..'/>
+            <input placeholder='vehicle name..' value={carName} onChange={handleCarName}/>
+            <input placeholder='vehicle image..' value={image} onChange={handleImage} />
+            <input placeholder='vehicle description..' value={description} onChange={handleDescription} />
+            <input placeholder='vehicle engine type..' value={engine} onChange={handleEngine} />
+            <input placeholder='vehicle transmission type..' value={transmission} onChange={handleTransmission} />
+            <input placeholder='vehicle drivetrain type..' value={drivetrain} onChange={handleDrivetrain} />
+            <input placeholder='vehicle power..' value={power} onChange={handlePower} />
+            <input placeholder='vehicle torque..' value={torque} onChange={handleTorque} />
+            <input placeholder='vehicle 0-60..' value={mph} onChange={handleMph} />
+            <input placeholder='vehicle 1/4 split..' value={quarterMile} onChange={handleQuarterMile} />
             <input type='submit' id='submit'/> 
             </div>
         </form>
