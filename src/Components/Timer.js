835e3a5910053da873ec6660eea6a322eecd7a92
Cedric <cedricj765@mail.com>
2023-10-06 07:05:43 -0500

    timer created and updated

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,22 +1,53 @@
-import React, { useState, useEffect} from 'react'
+import React, { useRef, useState, useEffect} from 'react'
 
 const Timer = () => {
 
-    const [ hours, setHours] = useState(24)
+    const [ hours, setHours] = useState('00')
 
-    const [ minutes, setMinutes] = useState(60)
+    const [ minutes, setMinutes] = useState('00')
 
-    const [ seconds, setSeconds] = useState(60)
+    const [ seconds, setSeconds] = useState('00')
+
+    let interval = useRef()
+
+    const startTimer = () => {
+
+      const  countDownDate = new Date('October 30, 2023 00:00:00').getTime();
+
+      interval = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = countDownDate - now
+
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 ))
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+        if (distance < 0) {
+          clearInterval(interval.current)
+
+        } else {
+          setHours(hours)
+          setMinutes(minutes)
+          setSeconds(seconds)
+        }
+
+      }, 1000)
+    }
 
     useEffect(() => {
 
-        const second = setTimeout(() => (prev => prev - 1), 1000)
+      startTimer()
+
+      return () => {
+        
+        clearInterval(interval.current)
+      }
     })
 
   return (
   
     <div>
-        hours:minutes:{seconds}
+        {hours}:{minutes}:{seconds}
     </div>
   )
 }
